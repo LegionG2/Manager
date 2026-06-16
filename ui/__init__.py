514766e642e3d34826d741466162fb7fc0bc4012
LegionG2 <283036924+LegionG2@users.noreply.github.com>
2026-06-16 16:45:30 +0200

    Enable configurable UI size

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,1 +1,9 @@
 """Tkinter UI package for Manager."""
+
+from .app import WorkshopApp
+from .ui_size_settings import enable_ui_size_settings
+
+
+enable_ui_size_settings(WorkshopApp)
+
+__all__ = ["WorkshopApp"]
