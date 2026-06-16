18f70ba96960ec892e4feaf72bd78096de2712a9
LegionG2 <283036924+LegionG2@users.noreply.github.com>
2026-06-16 16:57:43 +0200

    Enable persistent main window layout

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,8 +2,10 @@
 
 from .app import WorkshopApp
 from .ui_size_settings import enable_ui_size_settings
+from .window_state_settings import enable_window_state_settings
 
 
 enable_ui_size_settings(WorkshopApp)
+enable_window_state_settings(WorkshopApp)
 
 __all__ = ["WorkshopApp"]
